3fa08dad2f9cce293d3f9ffc4ba7f55bcf49a678
Dao Hoang Son <daohoangson@gmail.com>
2018-11-29 05:52:41 +0700

    Fix incomplete types.db

--- build-and-push.sh
+++ build-and-push.sh
@@ -5,7 +5,7 @@ set -e
 # https://pkgs.alpinelinux.org/packages?name=collectd&branch=v3.8
 LATEST_VERSION='5.8.0-r1'
 DOCKER_HUB_IMAGE='xfrocks/collectd'
-DOCKER_HUB_IMAGE_WITH_TAG="${DOCKER_HUB_IMAGE}:5.8.0"
+DOCKER_HUB_IMAGE_WITH_TAG="${DOCKER_HUB_IMAGE}:5.8.0b"
 
 git submodule update --init --recursive
 docker build --build-arg COLLECTD_VERSION="${LATEST_VERSION}" \
